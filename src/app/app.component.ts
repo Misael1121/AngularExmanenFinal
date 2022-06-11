58e346a63d7176827c420790a4df693fd07e07c5
MisaelMC11 <73720525+Misael1121@users.noreply.github.com>
2022-06-11 15:31:05 -0600

    Version 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'CI-CD ESTA ACTIVO EN AngularExamenFinalUMG';
+  title = 'CI-CD ESTA ACTIVO EN AngularExamenFinalUMG2022';
 }
